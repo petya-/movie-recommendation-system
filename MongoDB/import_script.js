7e3fc6130b1f3dcb96a78b991bbb473985178e86
Toms Uluks <ulukst@gmail.com>
2018-12-17 22:05:13 +0200

    fixed mongo import

--- MongoDB/import_script.js
+++ MongoDB/import_script.js
@@ -1,11 +1,6 @@
 
-let exec = require('child_process').exec
-let command1 = 'mongoimport -d recomendationSystems -c movies --type CSV --file ml-latest/movies.csv --headerline'
-let command2 = 'mongoimport -d recomendationSystems -c ratings --type CSV --file ml-latest/ratings.csv --headerline'
-let command3 = 'mongoimport -d recomendationSystems -c links --type CSV --file ml-latest/links.csv --headerline'
-exec(command1,command2,command3,  (err, stdout, stderr) => {
-  child_process.execSync(command1[options]);
-  child_process.execSync(command2[options]);
-  child_process.execSync(command3[options]);
-})
+let exec = require('child_process').exec;
+exec('mongoimport -d recomendationSystems -c movies --type CSV --file ../ml-latest/movies.csv --headerline');
+exec('mongoimport -d recomendationSystems -c ratings --type CSV --file ../ml-latest/ratings.csv --headerline');
+exec('mongoimport -d recomendationSystems -c links --type CSV --file ../ml-latest/links.csv --headerline');
 
