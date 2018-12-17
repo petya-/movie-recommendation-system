3f8fb81ccb5f1363ef4a44bbf98e2f564af958ef
Petya Buchkova <petya.buchkova@gmail.com>
2018-12-17 23:26:43 +0200

    Pushing final solution

--- PostgreSQL/setupDB.sql
+++ PostgreSQL/setupDB.sql
@@ -1,5 +1,5 @@
--- Create Database: lhrDB
 
+-- Create Database: movieSystem
 CREATE DATABASE "movieSystem"
     WITH
     OWNER = postgres
@@ -10,155 +10,195 @@ CREATE DATABASE "movieSystem"
 -- Connect to the database
 \connect movieSystem
 
--- Add needed extensions
-
--- create extension cube;
--- create extension fuzzystrmatch;
--- create extension pg_trgm;
--- create extension pgcrypto;
-
-
 -- Create tables
-
 CREATE TABLE users (
-    id SERIAL  NOT NULL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     email text UNIQUE,
     password text,
     firstname text,
     lastname text,
     enabled boolean DEFAULT TRUE,
-    "createdAt" timestamp DEFAULT now(),
-    "updatedAt" timestamp DEFAULT now()
+    createdAt timestamp DEFAULT now(),
+    updatedAt timestamp DEFAULT now()
 );
-
--- CREATE TABLE access_tokens (
---     id SERIAL PRIMARY KEY,
---     ttl integer,
---     userId integer REFERENCES users NOT NULL,
---     createdAt timestamp DEFAULT now(),
---     updatedAt timestamp DEFAULT now()
--- );
-
--- CREATE TABLE genres (
---     id SERIAL PRIMARY KEY,
---     name text UNIQUE,
---     position integer,
---     createdAt timestamp DEFAULT now(),
---     updatedAt timestamp DEFAULT now()
-
--- );
 
 CREATE TABLE movies (
-    id SERIAL NOT NULL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     title text NOT NULL,
-    genres text,
-    "createdAt" timestamp DEFAULT now(),
-    "updatedAt" timestamp DEFAULT now()
+    genres text NOT NULL,
+    createdAt timestamp DEFAULT now(),
+    updatedAt timestamp DEFAULT now()
 );
--- CREATE TABLE actors (
---     id SERIAL PRIMARY KEY,
---     name text,
---     createdAt timestamp DEFAULT now(),
---     updatedAt timestamp DEFAULT now()
--- );
--- CREATE TABLE movies_actors (
---     movie_id integer REFERENCES movies NOT NULL,
---     actor_id integer REFERENCES actors NOT NULL,
---     UNIQUE (movie_id, actor_id),
---     createdAt timestamp DEFAULT now(),
---     updatedAt timestamp DEFAULT now()
--- );
--- CREATE TABLE user_movies_lists (
---     id SERIAL PRIMARY KEY,
---     name text,
---     movie_id integer REFERENCES movies NOT NULL,
---     user_id integer REFERENCES users NOT NULL,
---     createdAt timestamp DEFAULT now(),
---     updatedAt timestamp DEFAULT now()
--- );
--- CREATE TABLE watched_movies (
---     id SERIAL PRIMARY KEY,
---     movie_id integer REFERENCES movies NOT NULL,
---     user_id integer REFERENCES actors NOT NULL,
---     createdAt timestamp DEFAULT now(),
---     updatedAt timestamp DEFAULT now()
--- );
 
 CREATE TABLE ratings (
     id SERIAL PRIMARY KEY,
-    rating double precision NOT NULL CHECK (rating BETWEEN 0. AND 5),
-    "movieId" integer REFERENCES movies NOT NULL,
-    "userId" integer REFERENCES users NOT NULL,
+    rating decimal NOT NULL CHECK (rating BETWEEN 0. AND 5),
+    movieId integer REFERENCES movies NOT NULL,
+    userId integer REFERENCES users NOT NULL,
     timestamp integer,
-    "createdAt" timestamp DEFAULT now(),
-    "updatedAt" timestamp DEFAULT now()
+    createdAt timestamp DEFAULT now(),
+    updatedAt timestamp DEFAULT now()
 );
 
 CREATE TABLE links (
     id SERIAL PRIMARY KEY,
-    "movieId" integer REFERENCES movies NOT NULL,
-    "imdbId" integer,
-    "tmdbId" integer,
-    "createdAt" timestamp DEFAULT now(),
-    "updatedAt" timestamp DEFAULT now()
+    movieId integer REFERENCES movies NOT NULL,
+    imdbId integer,
+    tmdbId integer,
+    createdAt timestamp DEFAULT now(),
+    updatedAt timestamp DEFAULT now()
+);
+
+CREATE TABLE tags (
+    userId integer REFERENCES users NOT NULL,
+    movieId integer REFERENCES movies NOT NULL,
+    tag text,
+    timestamp integer,
+    createdAt timestamp DEFAULT now(),
+    updatedAt timestamp DEFAULT now()
 );
 
 CREATE TABLE tmp (
-    "userId" integer,
-    "movieId" integer,
-    rating double precision,
+    userId integer,
+    movieId integer,
+    rating text,
     timestamp integer
 );
 
--- -- Import data in table from csv
--- CREATE OR REPLACE FUNCTION importFromCSV( table text, filePath text )
--- RETURNS table(id integer) AS $$
--- BEGIN
-
---   RETURN QUERY EXECUTE
---   \COPY tmp("userId","movieId", rating, timestamp) FROM '/Users/petyabuchkova/Desktop/KEA/Databases/movieSystem/storage/ratings.csv' WITH CSV HEADER;
-
---   'SELECT u.id
---   FROM users u, (SELECT user_id FROM watched_movies WHERE movie_id = $1) wm
---   WHERE u.id = wm.user_id
---   AND u.id != $2'
---   USING movie_id, user_id;
-
--- END;
--- $$ LANGUAGE plpgsql;
-
--- Copy the data from the CSV files
--- You can cope by converting property types. We normalized the database. We needed to match keys
-
+-- Copy the data from the CSV files into movies and tmp
 \COPY movies(id, title, genres) FROM '/Users/petyabuchkova/Desktop/KEA/Databases/movieSystem/storage/movies.csv' WITH CSV HEADER;
-\COPY tmp("userId","movieId", rating, timestamp) FROM '/Users/petyabuchkova/Desktop/KEA/Databases/movieSystem/storage/ratings.csv' WITH CSV HEADER;
+\COPY tmp(userId,movieId, rating, timestamp) FROM '/Users/petyabuchkova/Desktop/KEA/Databases/movieSystem/storage/ratings.csv' WITH CSV HEADER;
 
 
-
+-- Sync user ids with ratings
 INSERT INTO users(id)
-SELECT "userId" from tmp
-ON CONFLICT DO NOTHING;
+SELECT DISTINCT userId from tmp;
 
--- DELETE temp
+-- DROP temp table
 DROP TABLE tmp;
 
--- Populate users before migrating other files
--- Replace the copy with a function
-\COPY ratings("userId","movieId", rating, timestamp) FROM '/Users/petyabuchkova/Desktop/KEA/Databases/movieSystem/storage/ratings.csv' WITH CSV HEADER;
-\COPY links("movieId","imdbId", "tmdbId") FROM '/Users/petyabuchkova/Desktop/KEA/Databases/movieSystem/storage/links.csv' WITH CSV HEADER;
+-- Copy the data from the CSV files into ratings and links
+\COPY ratings(userId, movieId, rating, timestamp) FROM '/Users/petyabuchkova/Desktop/KEA/Databases/movieSystem/storage/ratings.csv' WITH CSV HEADER;
+\COPY links(movieId, imdbId, tmdbId) FROM '/Users/petyabuchkova/Desktop/KEA/Databases/movieSystem/storage/links.csv' WITH CSV HEADER;
+\COPY tags(userId, movieId, tag, timestamp) FROM '/Users/petyabuchkova/Desktop/KEA/Databases/movieSystem/storage/tags.csv' WITH CSV HEADER;
 
--- Populate users
+-- Sync user ids with tags
+INSERT INTO users(id)
+SELECT DISTINCT userId FROM tags
+ON CONFLICT (id) DO NOTHING;
 
 -- Create indexes
--- CREATE INDEX movies_actors_movie_id ON movies_actors (movie_id);
--- CREATE INDEX movies_actors_actor_id ON movies_actors (actor_id);
--- CREATE INDEX watched_movies_movie_id ON watched_movies (movie_id);
--- CREATE INDEX watched_movies_user_id ON watched_movies (user_id);
--- CREATE INDEX user_movies_lists_movie_id ON user_movies_lists (movie_id);
--- CREATE INDEX user_movies_lists_user_id ON user_movies_lists (user_id);
--- CREATE INDEX ratings_movie_id ON ratings (movie_id);
--- CREATE INDEX ratings_user_id ON ratings (user_id);
--- CREATE INDEX movies_title_pattern ON movies (lower(title) text_pattern_ops);
--- CREATE INDEX movies_title_trigram ON movies USING gist(title gist_trgm_ops);
--- CREATE INDEX movies_title_searchable ON movies USING gin(to_tsvector('english', title));
--- CREATE INDEX movies_genres_cube ON movies USING gist (genre);
+CREATE INDEX movies_genre ON movies (genres);
+CREATE INDEX movies_title ON movies (title);
+CREATE INDEX ratings_user ON ratings (userId);
+CREATE INDEX ratings_movie ON ratings (movieId);
+CREATE INDEX links_movie ON links (movieId);
+
+--------------------  Demonstrate basic CRUD functionality of new movies and ratings --------------------
+
+-- Set movie id sequence
+SELECT setval('movies_id_seq', (SELECT MAX(id) FROM "movies"));
+
+-- Create movie
+INSERT INTO movies (title, genres)
+VALUES
+    ('The French Connection','Documentary'),
+    ('Blowup','Comedy|Action'),
+    ('Breathless','Thriller'),
+    ('Arlington Road','Comedy|Romance|Drama'),
+    ('Point Break','Fiction|Comedy');
+-- Get movie
+SELECT title
+FROM movies
+WHERE title = 'Blowup';
+-- Update movie
+UPDATE movies
+SET title = 'Blowup 2'
+WHERE title = 'Blowup';
+-- Delete movie
+DELETE FROM movies
+WHERE title = 'Blowup 2';
+
+-- Create rating
+INSERT INTO ratings (rating, movieId, userId, timestamp)
+VALUES (4.5, 2, 14, 1234);
+-- Get rating
+SELECT rating
+FROM ratings
+WHERE rating = 4.5
+AND movieId = 2
+AND userId = 14;
+-- Update rating
+UPDATE ratings
+SET rating = 3
+WHERE rating = 4.5
+AND userId = 14
+AND movieId = 2;
+-- Delete rating
+DELETE FROM ratings
+WHERE rating = 3
+AND movieId = 2
+AND userId = 14;
+
+
+-- Recommend movies based on genres function
+CREATE OR REPLACE FUNCTION find_same_genre_movies(movieId integer, limiter integer)
+    RETURNS TABLE (title text, genres text)
+AS $$
+DECLARE genresArr text[] := (SELECT string_to_array(movies.genres, '|') FROM movies where id =  $1);
+BEGIN
+    RETURN query
+    SELECT m.title, m.genres
+    FROM movies AS m
+    WHERE string_to_array(m.genres, '|') && genresArr
+    AND m.id != $1
+    LIMIT $2;
+END;
+$$
+LANGUAGE plpgsql;
+
+SELECT title, genres FROM movies WHERE id=10;
+SELECT find_same_genre_movies(10, 7);
+
+-- Recommend not rated movies to a user function
+CREATE OR REPLACE FUNCTION find_non_rated_movies (userId integer, limiter integer)
+    RETURNS TABLE (id integer, title text)
+AS $$
+DECLARE
+ ratedMoviesIds integer[] := ARRAY(SELECT r.movieId FROM ratings as r WHERE r.userId = $1);
+BEGIN
+    RETURN query
+    SELECT m.id, m.title
+    FROM movies as m
+    WHERE m.id <> ALL (ARRAY[ratedMoviesIds])
+    LIMIT $2;
+END;
+$$
+LANGUAGE plpgsql;
+
+SELECT movieId FROM ratings WHERE userId = 6 LIMIT 5;
+SELECT find_non_rated_movies(6, 10);
+
+-- Search movies with input errors
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
+
+CREATE INDEX movies_title_trigram ON movies USING gist (title gist_trgm_ops);
+
+CREATE OR REPLACE FUNCTION search_movie_by_title (movieTitle text)
+  RETURNS TABLE (title text) AS $$
+    BEGIN
+        RETURN query
+        SELECT m.title
+        FROM movies as m
+        WHERE m.title % movieTitle;
+    END;
+    $$ LANGUAGE plpgsql;
+
+SELECT search_movie_by_title('Stanby');
+SELECT search_movie_by_title('Ballerma');
+SELECT search_movie_by_title('a hose of secrets expolre');
+
+-- FUTURE IMPLEMENTATION --
+-- Populate users table and apply constraint:
+-- ALTER TABLE users ALTER COLUMN email SET UNIQUE NOT NULL;
+-- ALTER TABLE users ALTER COLUMN password SET UNIQUE NOT NULL;
