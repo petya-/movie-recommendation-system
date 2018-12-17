fe16b58a406489a9eda38164113010a9b6f72cda
Toms Uluks <ulukst@gmail.com>
2018-12-17 21:20:30 +0200

    upgraded functions

--- MongoDB/functions.js
+++ MongoDB/functions.js
@@ -1,7 +1,20 @@
-function getMoviesByGenre(movieID) {
+function getMoviesByAllGenre(movieID) {
     var movie = db.movies.findOne({'movieId': movieID}, { _id : 0, genres : 1 })
     if (movie.genres) {
         return db.movies.find({'genres': movie.genres })
+    } else {
+        throw Error('Movie has no genres')
+    }
+}
+
+function getMoviesByOneGenre(movieID) {
+    var movie = db.movies.findOne({'movieId': movieID}, { _id : 0, genres : 1 })
+    if (movie.genres) {
+        var genres = movie.genres.split('|');
+        for (let index = 0; index < genres.length; index++) {
+            return db.movies.find({'genres': {$regex: genres[index]} })
+            
+        }
     } else {
         throw Error('Movie has no genres')
     }
@@ -16,15 +29,29 @@ function getMoviesByRating(userID) {
     return db.movies.find({'movieId': {$nin: movieId}})
 }
 
-function searchMovie(movieName) {
-    
+function searchMovie() { 
+    return db.movies.find({ title : {$regex: 'Powder'}})
+    .explain("executionStats").executionStats
+}
+
+function titleIndex() {
+    db.movies.dropIndex( { title : 1 });
 }
 
 function addReview(userID, movieID, text ) {
-    db.reviews.insert({
-        'userId': userID,
-        'movieId': movieID,
-        'review_text': text,
-        'timestamp':  $currentDate
-    })
+    if (userID, movieID, text) {
+        db.reviews.insert({
+            'userId': userID,
+            'movieId': movieID,
+            'review_text': text,
+            'timestamp':  Date()
+        })
+    } else {
+        throw Error('Some fields are missing')
+    }
 }
+
+
+        function getReviewsForMovie(movieID) {
+            return db.reviews.find({'movieId': movieID});
+        }
--- MongoDB/import_script.js
+++ MongoDB/import_script.js
@@ -4,7 +4,8 @@ let command1 = 'mongoimport -d recomendationSystems -c movies --type CSV --file 
 let command2 = 'mongoimport -d recomendationSystems -c ratings --type CSV --file ml-latest/ratings.csv --headerline'
 let command3 = 'mongoimport -d recomendationSystems -c links --type CSV --file ml-latest/links.csv --headerline'
 exec(command1,command2,command3,  (err, stdout, stderr) => {
-  // check for errors or if it was succesfuly
-  cb()
+  child_process.execSync(command1[options]);
+  child_process.execSync(command2[options]);
+  child_process.execSync(command3[options]);
 })
 
